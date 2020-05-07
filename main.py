637e75ffb076f96926042b510137ea52169a30b1
AgusQuintanar <agusquintanar17@gmail.com>
2020-05-06 23:53:32 -0500

    create main functions

--- main.py
+++ main.py
@@ -53,65 +53,72 @@ def imprimir_arbol_de_derivacion(root):
                 nivel.append(node)
             recorrido.append(node.value)
          
-recorrido = []
-entradas = []
-
-entradas.append("S->AB|SS|AC")
-entradas.append("A->[")
-entradas.append("B->]")
-entradas.append("C->SB")
-
-cadena = list("[[][]]")
-print("cadena", cadena)
-n = len(cadena)
-
-producciones = {}
-generadores = {}
-
-for e in entradas: #llenado de diccionario de producciones
-    generador, terminales = e.split("->")
-    for terminal in terminales.split("|"):
-        producciones[terminal] = generador
-        generadores[generador] = terminal
+def create_dictionaty():
+    for e in entradas: #llenado de diccionario de producciones
+        generador, terminales = e.split("->")
+        for terminal in terminales.split("|"):
+            producciones[terminal] = generador
+            generadores[generador] = terminal
 
 
-matriz_sub = [[["0",None, None] for y in range(n)] for x in range(n)] #cracion de matriz de subcadenas
+def create_matrix():
+    matriz_sub = [[["0",None, None] for y in range(n)] for x in range(n)] #cracion de matriz de subcadenas
+    for i in range(n):
+        matriz_sub[i][i][0] = producciones[cadena[i]]
 
-for i in range(n):
-    matriz_sub[i][i][0] = producciones[cadena[i]]
-
-
-for i in range(1, n):
-    for j in range(i, n):
-        for k in range(i):
-            p_1 = matriz_sub[j-i][j-i+k][0]
-            p_2 = matriz_sub[j-i+k+1][j][0]
-            produccion = obtener_produccion(p_1, p_2)
-            if produccion in producciones:
-                matriz_sub[j-i][j] = [producciones[produccion], [j-i, j-i+k], [j-i+k+1, j]]
-                break
-            elif k == i-1:
-                matriz_sub[j-i][j][0] = None
-
-print("\n")
-imprimir_matriz(matriz_sub)
-print("\n")
-
-if matriz_sub[0][n-1][0] == "S":
-    print("La cadena si pertenece a la Gramatica")
-    generar_arbol_de_derivacion(matriz_sub, n)
-
-    recorrido_str = ""
-    for x in recorrido:
-        if x == "null":
-            recorrido_str += "null,"
-        else:
-            recorrido_str += str(ord(x)) + ","
-            #recorrido_str += str(x) + ","
-
-    print('['+recorrido_str.rstrip(',')+']')
-    bt.drawtree(bt.deserialize('['+recorrido_str.rstrip(',')+']'))
-else:
-    print("La cadena no pertene a la gramatica")
+def generate_CYK():
+    for i in range(1, n):
+        for j in range(i, n):
+            for k in range(i):
+                p_1 = matriz_sub[j-i][j-i+k][0]
+                p_2 = matriz_sub[j-i+k+1][j][0]
+                produccion = obtener_produccion(p_1, p_2)
+                if produccion in producciones:
+                    matriz_sub[j-i][j] = [producciones[produccion], [j-i, j-i+k], [j-i+k+1, j]]
+                    break
+                elif k == i-1:
+                    matriz_sub[j-i][j][0] = None
 
 
+def check_for_valid_grammar(matriz_sub, recorrido):
+    if matriz_sub[0][n-1][0] == "S":
+        print("La cadena si pertenece a la Gramatica")
+        generar_arbol_de_derivacion(matriz_sub, n)
+
+        recorrido_str = ""
+        for x in recorrido:
+            if x == "null":
+                recorrido_str += "null,"
+            else:
+                recorrido_str += str(ord(x)) + ","
+                #recorrido_str += str(x) + ","
+
+        print('['+recorrido_str.rstrip(',')+']')
+        bt.drawtree(bt.deserialize('['+recorrido_str.rstrip(',')+']'))
+    else:
+        print("La cadena no pertene a la gramatica")
+
+
+def main():
+    recorrido = []
+    entradas = []
+
+    entradas.append("S->AB|SS|AC")
+    entradas.append("A->[")
+    entradas.append("B->]")
+    entradas.append("C->SB")
+
+    cadena = list("[[][]]")
+    print("cadena", cadena)
+    n = len(cadena)
+
+    producciones = {}
+    generadores = {}
+    create_dictionaty()
+    create_matrix()
+
+    print("\n")
+    imprimir_matriz(matriz_sub)
+    print("\n")
+
+    check_for_valid_grammar(matriz_sub, recorrido)
